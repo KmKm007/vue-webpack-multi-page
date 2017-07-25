cbcc6012fd3f201a8084391020790dffe3560ca7
KmKm <kmkmhuang007@gmail.com>
2017-07-25 13:14:53 +0800

    update

--- src/module/home/index.js
+++ src/module/home/index.js
@@ -1,5 +1,5 @@
 const pathname = window.location.pathname
 var split = pathname.split('/')
-if (split.length === 2) {
+if (pathname !== '/' && split.length === 2) {
   window.location.href = split[1] + '.html'
 }
